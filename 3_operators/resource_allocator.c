9d54d3f2a3721b881f830cd2a653a34b4252ef4b
Firdaus <firdaus1337@gmail.com>
2025-05-09 22:01:42 +0800

    fix

--- 3_operators/resource_allocator.c
+++ 3_operators/resource_allocator.c
@@ -14,15 +14,15 @@ int main()
     printf("Total Resource Units: %d\n\n", resources);
 
     printf("TeamDistribution:\n");
-    printf("Each of the %d teams receives: %d units\n");
+    printf("Each of the %d teams receives: %d units\n", teamCount, resourcesPerTeam);
     printf("Units remaining after team distribution: %d\n\n", undistributedResources);
 
     int completelyFilledCargo = resources / maxResourcePerContainer;
     int lastCargoTotalResource = resources % maxResourcePerContainer;
 
     printf("Container Packing (Capacity: %d units/container):\n", maxResourcePerContainer);
-    printf("Number of full containers needed: %d containers", completelyFilledCargo);
-    printf("Units in the last (partially filled) container: %d units", lastCargoTotalResource);
+    printf("Number of full containers needed: %d containers\n", completelyFilledCargo);
+    printf("Units in the last (partially filled) container: %d units\n", lastCargoTotalResource);
 
     return 0;
 }
