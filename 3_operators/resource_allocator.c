5a7e48993b33faeac0f0453d0e0a099d8f80e68a
Firdaus <firdaus1337@gmail.com>
2025-05-09 22:34:43 +0800

    format all file

--- 3_operators/resource_allocator.c
+++ 3_operators/resource_allocator.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+  int resources = 257;
+  int teamCount = 5;
+  int maxResourcePerContainer = 50;
 
-    int resources = 257;
-    int teamCount = 5;
-    int maxResourcePerContainer = 50;
+  int resourcesPerTeam = resources / teamCount;
+  int undistributedResources = resources % teamCount;
 
-    int resourcesPerTeam = resources / teamCount;
-    int undistributedResources = resources % teamCount;
+  printf("--- Resource Allocation Report ---\n\n");
+  printf("Total Resource Units: %d\n\n", resources);
 
-    printf("--- Resource Allocation Report ---\n\n");
-    printf("Total Resource Units: %d\n\n", resources);
+  printf("TeamDistribution:\n");
+  printf("Each of the %d teams receives: %d units\n", teamCount, resourcesPerTeam);
+  printf("Units remaining after team distribution: %d\n\n", undistributedResources);
 
-    printf("TeamDistribution:\n");
-    printf("Each of the %d teams receives: %d units\n", teamCount, resourcesPerTeam);
-    printf("Units remaining after team distribution: %d\n\n", undistributedResources);
+  int completelyFilledCargo = resources / maxResourcePerContainer;
+  int lastCargoTotalResource = resources % maxResourcePerContainer;
 
-    int completelyFilledCargo = resources / maxResourcePerContainer;
-    int lastCargoTotalResource = resources % maxResourcePerContainer;
+  printf("Container Packing (Capacity: %d units/container):\n", maxResourcePerContainer);
+  printf("Number of full containers needed: %d containers\n", completelyFilledCargo);
+  printf("Units in the last (partially filled) container: %d units\n", lastCargoTotalResource);
 
-    printf("Container Packing (Capacity: %d units/container):\n", maxResourcePerContainer);
-    printf("Number of full containers needed: %d containers\n", completelyFilledCargo);
-    printf("Units in the last (partially filled) container: %d units\n", lastCargoTotalResource);
-
-    return 0;
+  return 0;
 }
