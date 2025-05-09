1795c171906c940801923d47df11a2dc496d358b
Firdaus <firdaus1337@gmail.com>
2025-05-09 22:33:50 +0800

    change format

--- 4_conditions/gate_access.c
+++ 4_conditions/gate_access.c
@@ -1,33 +1,30 @@
 #include <stdio.h>
 
-int main()
-{
-    int accessLevel = 7;
-    int isRestrictedHours = 0;
+int main() {
+  int accessLevel = 10;
+  int isRestrictedHours = 1;
 
-    if (accessLevel > 0 && accessLevel < 10)
-    {
-        if (accessLevel == 10)
-        {
-            printf("Access Granted: Welcome, Admin!\n");
-        }
-        else if (accessLevel > 4 && accessLevel < 10 && !isRestrictedHours)
-        {
-            printf("Access Granted: Welcome, Researcher!\n");
-        }
-        else if (accessLevel < 5 && !isRestrictedHours)
-        {
-            printf("Access Granted: Welcome, Intern!\n");
-        }
-        else
-        {
-            printf("Access DEnied: Restricted Hours Active for Your Level.\n");
-        }
-    }
-    else
-    {
-        printf("Access DEnied: Invalid Access Level.\n");
-    }
+  printf("Input: Level %d, Restricted Hours: %d\n", accessLevel, isRestrictedHours);
 
-    return 0;
+  if (accessLevel == 10) {
+    printf("Access Granted: Welcome, Admin!\n");
+  } else if (accessLevel >= 5 && accessLevel <= 9) {
+    if (!isRestrictedHours) {
+      printf("Access Granted: Welcome, Researcher!\n");
+    } else {
+      printf("Access Denied: Restricted Hours Active for Your Level.\n");
+    }
+  } else if (accessLevel >= 1 && accessLevel <= 4) {
+    if (!isRestrictedHours) {
+      printf("Access Granted: Welcome, Intern!\n");
+    } else {
+      printf("Access Denied: Restricted Hours Active for Your Level.\n");
+    }
+  } else if (accessLevel == 0) {
+    printf("Access Denied: Authorization Level Insufficient.\n");
+  } else {
+    printf("Access Denied: Invalid Access Level.\n");
+  }
+
+  return 0;
 }
