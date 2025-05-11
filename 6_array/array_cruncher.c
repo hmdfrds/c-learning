941189ccf740e206478979237eccc8d3951bc5da
hmdfrds <firdaus1337@gmail.com>
2025-05-12 04:47:53 +0800

    array

--- 6_array/array_cruncher.c
+++ 6_array/array_cruncher.c
@@ -1,24 +1,45 @@
 #include <limits.h>
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int data[] = {27, 82, 45, 19, 63, 91, 8, 53, 76, 30};
     int arrLen = sizeof(data) / sizeof(data[0]);
     int sum = 0;
     int max = INT_MIN;
     int min = INT_MAX;
+    int g50 = 0;
 
-    for (int i = 0; i < arrLen; i++) {
+    for (int i = 0; i < arrLen; i++)
+    {
         printf("%d ", data[i]);
         sum += data[i];
-        if (data[i] > max) {
-            max = data;
+        if (data[i] > max)
+        {
+            max = data[i];
         }
-        if (data[i] < min) {
-            min = data;
+        if (data[i] < min)
+        {
+            min = data[i];
+        }
+        if (data[i] > 50)
+        {
+            g50++;
         }
     }
     float avg = (float)sum / arrLen;
+
+    printf("--- Array Data Alaysis ---\n");
+    printf("Array Elements: ");
+    for (int i = 0; i < arrLen; i++)
+    {
+        printf("%d ", data[i]);
+    }
+    printf("Sum of Elements: %d\n", sum);
+    printf("Average of Elements: %.2f\n", avg);
+    printf("Maximum Value: %d\n", max);
+    printf("Minimux Value: %d\n", min);
+    printf("Elements Greater Than 50: %d", g50);
 
     return 0;
 }
