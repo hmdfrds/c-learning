014c73b72bcdab8785be901aeb5231e4d0f57d53
firdaus <firdaus1337@gmail.com>
2025-07-17 17:40:42 +0800

    flow

--- test/hello.c
+++ test/hello.c
@@ -18,5 +18,16 @@ int main() {
   printf("PI: %.2f\n", pi);
   printf("Initial: %c\n", initial);
 
+  if (age >= 18) {
+    printf("You're an adult!\n");
+  } else {
+    printf("Still a kid.\n");
+  }
+
+  int count = 0;
+  while (count < 3) {
+    printf("Loop #%d\n", ++count);
+  }
+
   return 0;
 }
